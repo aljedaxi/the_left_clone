67534c40051128c6ef38fbc40dbec13c9ee90e93
aljedaxi <aljedaxi@gmail.com>
2019-09-30 11:49:49 -0600

    can generate syntactically correct messages

--- event_classes/all.js
+++ event_classes/all.js
@@ -1,19 +1,19 @@
-/*
+/**
   @file event schemata
   @copyright fuck off
   @author daxi
 */
-
-class MalformedMetaData extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'MalformedMetaData';
-  }
-}
+/*
+  fyi, gatherings work by creating an {EventDeclaration} message; 
+  that message doesn't contain any information of its own.
+  Information about the event is contained within various "about" messages,
+  which link back to the original {EventDeclaration}.
+ */
 
 const org = 'ryuuko';
 const app = 'the_left';
-/*
+
+/**
   @returns content portion of a gathering declaration
 */
 function EventDeclaration() {
@@ -22,55 +22,49 @@ function EventDeclaration() {
   };
 }
 
+/**
+  * this is literally just here to describe what {Event} needs
+ */
 class EventMeta {
-  constructor(
-    event_id, 
-    title, 
-    description, 
-    event_location, 
-    startDateTime, 
-    endDateTime, 
-    contributors,
-    hosts,
-    attendees,
-    images,
-    address,
-  ) {
-    this.event_id = event_id, 
-    this.title = title, 
-    this.description = description, 
-    this.event_location = event_location, 
-    this.startDateTime = startDateTime, 
+  constructor(event_id, title, description, event_location, startDateTime, endDateTime, contributors, hosts, attendees, images, address) {
+    if (!event_id) {
+      throw SyntaxError('this is metadata; it must refer to data');
+    }
+    this.event_id = event_id;
+    this.title = title;
+    this.description = description;
+    this.event_location = event_location;
+    this.startDateTime = startDateTime;
     if(endDateTime) {
-      this.endDateTime = endDateTime, 
+      this.endDateTime = endDateTime;
     }
     if(contributors) {
-      this.contributors = contributors,
+      this.contributors = contributor;
     }
     if(hosts) {
-      this.hosts = hosts,
+      this.hosts = host;
     }
     if(attendees) {
-      this.attendees = attendees,
+      this.attendees = attendee;
     }
     if(images) {
-      this.images = images,
+      this.images = image;
     }
     if(address) {
-      this.address = address,
+      this.address = addres;
     }
   }
 }
 
-/*
+/**
+  @description generates the content portion of an gathering
   @returns content portion of a message detailing gathering metadata
   @param {EventMeta} event_meta
   details the async part of https://github.com/pietgeursen/patch-gatherings
  */
-
-function Event(event_meta, event_id) {
-  if (!(event_meta.title && event_meta.location && event_meta.description && (event_id || event_meta.event_id) ) ) {
-    throw new MalformedMetaData('the event metadata you passed lacked a title, location, description, or event_id.');
+function Event(event_meta) {
+  if(!event_meta.event_id) {
+    throw new SyntaxError('metadata must be meta to data. call this with an event_id');
   }
 
   const decompose_date_time = time => {
@@ -84,10 +78,14 @@ function Event(event_meta, event_id) {
 
   this.timezone = event_meta.timezone || Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  event_id = event_id || event_meta.event_id;
+  event_id = event_meta.event_id;
+  delete event_meta.event_id;
+
+  //these things are mandatory
   event_meta.type = 'about';
   event_meta.about = event_id;
   event_meta.branch = event_id; //don't actually know what this means
+  //these things need processing
   if (event_meta.start_time) {
     event_meta.startDateTime = decompose_date_time(start_time, event_timezone);
   }
@@ -97,6 +95,52 @@ function Event(event_meta, event_id) {
   if (event_meta.address) {
     event_meta.geometry = this.geocode(address);
   }
-    
+  //everything else is already in ${event_meta}
   return event_meta;
 }
+
+function OpeningEvent(event_id, title, event_location, description, event_meta) {
+  if (!(title && event_location && description && event_id) ) {
+    throw new SyntaxError('the event metadata you passed lacked a title, location, description, or event_id.');
+  }
+
+  event_meta = event_meta || {};
+  Object.assign(event_meta, {
+    event_id,
+    title,
+    event_location,
+    description,
+  });
+
+  return new Event(event_meta);
+}
+
+// https://ssbc.github.io/scuttlebutt-protocol-guide/
+function Message(previous, author, sequence, content) {
+  return {
+    previous,
+    author,
+    sequence,
+    content,
+    hash: 'sha256',
+    timestamp: new Date().getTime(),
+  };
+}
+
+const me = "@3P07vY/2X2ErDk9tJPhMgyyJdPGWfnP3CgazkMYc66E=.ed25519";
+const test_Message = Message.bind(null, null, me, 1);
+
+//const e = new EventMeta(5, 'meme', 
+
+//test
+const fail_EventMeta = () => new EventMeta();
+
+const fail_Event = () => new Event({title: 'meme'});
+
+const fail_OpeningEvent = () => new OpeningEvent();
+
+const basic_event = new OpeningEvent(5, 'party', "Jules'", "party");
+
+const basic_message = new test_Message(basic_event);
+
+console.log(basic_message);
