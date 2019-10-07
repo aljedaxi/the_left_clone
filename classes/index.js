415f8ca472dc31b4470793cbbe39c1999efbde3d
aljedaxi <aljedaxi@gmail.com>
2019-10-07 17:03:24 -0600

    created a real npm package

---
+++ classes/index.js
@@ -0,0 +1,8 @@
+const gatherings_classes = require('gatherings.js');
+const map_controller_queries = require('gatherings.js');
+
+//dead simple for now
+module.exports = {
+  gathering_classes,
+  map_controller_queries,
+};
